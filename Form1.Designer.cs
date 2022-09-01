a2455670b287820f01c039a47a8f33d71053dc9b
Abiner Arefaine <109365314+Abiner251@users.noreply.github.com>
2022-09-01 20:21:20 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace timerAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer500 = new System.Windows.Forms.Timer(this.components);
+            this.timer1000 = new System.Windows.Forms.Timer(this.components);
+            this.timer1500 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lavender;
+            this.label1.Location = new System.Drawing.Point(129, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 188);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lavender;
+            this.label2.Location = new System.Drawing.Point(578, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 188);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lavender;
+            this.label3.Location = new System.Drawing.Point(350, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 188);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Lavender;
+            this.label4.Location = new System.Drawing.Point(266, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 188);
+            this.label4.TabIndex = 3;
+            this.label4.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Lavender;
+            this.label5.Location = new System.Drawing.Point(490, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 188);
+            this.label5.TabIndex = 4;
+            this.label5.Text = ":";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 53);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(453, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 53);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer500
+            // 
+            this.timer500.Interval = 50;
+            this.timer500.Tick += new System.EventHandler(this.timer500_Tick);
+            // 
+            // timer1000
+            // 
+            this.timer1000.Tick += new System.EventHandler(this.timer1000_Tick);
+            // 
+            // timer1500
+            // 
+            this.timer1500.Interval = 150;
+            this.timer1500.Tick += new System.EventHandler(this.timer1500_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(874, 521);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer500;
+        private System.Windows.Forms.Timer timer1000;
+        private System.Windows.Forms.Timer timer1500;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timerAssignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer500_Tick(object sender, EventArgs e)
+        {
+            
+            int time = Convert.ToInt32(label1.Text);
+            time++;
+            if (time > 9)
+            {
+                time = 0;
+            }
+            
+            label1.Text = (time).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer500.Start();
+            timer1000.Start();
+            timer1500.Start();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            int time = Convert.ToInt32(label3.Text);
+            time++;
+            if (time > 9)
+            {
+                time = 0;
+            }
+
+            label3.Text = (time).ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1000_Tick(object sender, EventArgs e)
+        {
+            int time = Convert.ToInt32(label3.Text);
+            time++;
+            if (time > 9)
+            {
+                time = 0;
+            }
+
+            label3.Text = (time).ToString();
+        }
+
+        private void timer1500_Tick(object sender, EventArgs e)
+        {
+            int time = Convert.ToInt32(label2.Text);
+            time++;
+            if (time > 9)
+            {
+                time = 0;
+            }
+
+            label2.Text = (time).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer500.Stop();
+            timer1000.Stop();
+            timer1500.Stop();
+        }
+    }
+}
